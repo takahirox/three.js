e5b9048685b5135f64f8a2ff8e759eb09430aecd
Takahiro <hogehoge@gachapin.jp>
2019-02-25 06:37:56 +0900

    Update WebGLProgram

--- src/renderers/webgl/WebGLProgram.js
+++ src/renderers/webgl/WebGLProgram.js
@@ -558,6 +558,8 @@ function WebGLProgram( renderer, extensions, code, material, shader, parameters,
 		// GLSL 3.0 conversion
 		prefixVertex = [
 			'#version 300 es\n',
+			extensions.get( 'WEBGL_multiview' ) !== null ? 'extension GL_OVR_multiview : require' : '',
+			extensions.get( 'WEBGL_multiview' ) !== null ? 'layout(num_views=2)' : '',
 			'#define attribute in',
 			'#define varying out',
 			'#define texture2D texture'
