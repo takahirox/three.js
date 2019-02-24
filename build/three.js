980c77a9572bb623f66c02f324c8aefd609d03e3
Takahiro <hogehoge@gachapin.jp>
2019-02-24 19:55:27 +0900

    Update build

--- build/three.js
+++ build/three.js
@@ -22346,7 +22346,7 @@
 				var viewportLeft = cameraVR.cameras[ 0 ].viewport;
 				var viewportRight = cameraVR.cameras[ 1 ].viewport;
 				return target.set( viewportLeft.x, viewportLeft.y,
-					viewportLeft.width + viewportRight.width, viewportLeft.height );
+					viewportLeft.z + viewportRight.z, viewportLeft.w );
 
 			} else {
 
