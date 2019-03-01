0fb0c3d8736c13809b53f0ef113686f95f28f974
Takahiro <hogehoge@gachapin.jp>
2019-03-02 01:52:31 +0900

    Fix conflict

--- src/renderers/webvr/WebVRManager.js
+++ src/renderers/webvr/WebVRManager.js
@@ -265,9 +265,10 @@ function WebVRManager( renderer ) {
 
 		var userHeight = frameOfReferenceType === 'stage' ? 1.6 : 0;
 
-		if ( device === null ) {
+		if ( isPresenting() === false ) {
 
 			camera.position.set( 0, userHeight, 0 );
+			camera.rotation.set( 0, 0, 0 );
 			return camera;
 
 		}
