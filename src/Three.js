3ba6915f0d8925ece7b66abf8b843d58609560b3
Takahiro <hogehoge@gachapin.jp>
2019-04-30 19:28:50 +0900

    Clean up

--- src/Three.js
+++ src/Three.js
@@ -1,7 +1,6 @@
 import './polyfills.js';
 
 export { WebGLMultisampleRenderTarget } from './renderers/WebGLMultisampleRenderTarget.js';
-export { WebGLMultiviewRenderTarget } from './renderers/WebGLMultiviewRenderTarget.js';
 export { WebGLRenderTargetCube } from './renderers/WebGLRenderTargetCube.js';
 export { WebGLRenderTarget } from './renderers/WebGLRenderTarget.js';
 export { WebGLRenderer } from './renderers/WebGLRenderer.js';
